096b68159dd80f7a4102c225a9bc3dd9ee79bcc9
Ghost91202 <karansharma8543@gmail.com>
2024-02-09 13:36:39 +0500

    add onclick change comp

--- src/app/about/comp/onclickchange.js
+++ src/app/about/comp/onclickchange.js
@@ -24,16 +24,21 @@ const ButtonComponent = () => {
 
   return (
     <div>
-      <h1>Button Component</h1>
-      {Object.keys(componentsMap).map((component) => (
-        <div className="flex gap-10">
+      <div className="flex flex-col gap-10">
+ <h1>onclick change Button Component</h1>
+        <div className="flex gap-5">
+            {Object.keys(componentsMap).map((component) => (
+        <div className="flex flex-row  text-green-500  gap-10">
            <button key={component} onClick={() => setSelectedComponent(component)}>
           {`Show ${component}`}
         </button>
        </div>
       ))}
+    </div>
 
       <div style={{ marginTop: '20px' }}>{selectedComponent && renderComponent(selectedComponent)}</div>
+
+</div>
     </div>
   );
 };
