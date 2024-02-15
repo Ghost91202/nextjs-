f79f09fe6e38fbaf8c3ea251080e0c9f6b72817b
ghost91202 <karansharma8543@gmail.com>
2024-02-16 00:15:53 +0530

    new

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -5,7 +5,7 @@ export default function hero1 ()  {
     return (
         <div className="p-2 w-screen overflow-x-hidden">
             <div className="h-xl">
-            <Image src={hero} alt="pie bond  digital marketing agency" className="w-full h-xl rounded-2xl" />
+            <Image src={hero} alt="pie bond  digital marketing agency" className="w-full h-screen  rounded-2xl" />
             </div>
             <div className=" flex flex-col font-extrabold pl-10  absolute z-10 text-white lg:text-8xl text-4xl justify-end -mt-20 ">
                 <div className="absolute">
