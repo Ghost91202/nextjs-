238166b683fba73be97a431a614557cd7b26e505
ghost91202 <karansharma8543@gmail.com>
2024-02-17 22:11:57 +0530

    fixed navbar issue

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ import Section6 from "../components/Sections/section6.js"
 import Section7 from "../components/Sections/section7.js"
 import Footer from '../components/footer/footer';
 import Faq from "../components/faq/faq"
-import Navbar from "@/components/navbar/Navbar"
+import Navbar from "../components/navbar/Navbar"
 import Quote from "../components/Sections/Quote"
 import Grid from "../components/Sections/Grid.jsx"
 import Cont from "../components/Sections/Connect"
