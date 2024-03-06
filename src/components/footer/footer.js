87c3d9697325a8d3569cbdbc45e2e7ae34527a9c
aakash <mahajanaakash108@gmail.com>
2024-03-07 00:14:27 +0530

    Colour of Arrow changed to Cyan

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -20,41 +20,41 @@ export default function footer( params ) {
                         <div >
                             {/* <h1 className="lg:text-xl text-cyan-400">Service</h1> */}
                             <ul className="flex flex-col">
-                                <Link className="footer-links group relative w-fit py-1"href="/services/appdevelopment">App Development <div className="absolute left-0 bottom-0 h-1 w-0 bg-white group-hover:w-full transition-all duration-500"></div> </Link>
-                                <Link className="footer-links group relative w-fit py-1"href="/services/bingads">Bing Ads<div className="absolute left-0 bottom-0 h-1 w-0 bg-white group-hover:w-full transition-all duration-500"></div> </Link>
-                                <Link className="footer-links group relative w-fit py-1"href="/services/contentmarketing">Content Marketing<div className="absolute left-0 bottom-0 h-1 w-0 bg-white group-hover:w-full transition-all duration-500"></div> </Link>
-                                <Link className="footer-links group relative w-fit py-1"href="/services/conversionrateoptimization">Conversion Rate Optimization<div className="absolute left-0 bottom-0 h-1 w-0 bg-white group-hover:w-full transition-all duration-500"></div> </Link>
-                                <Link className="footer-links group relative w-fit py-1"href="/services/digitalmarketing">Digital Marketing<div className="absolute left-0 bottom-0 h-1 w-0 bg-white group-hover:w-full transition-all duration-500"></div> </Link>
+                                <Link className="footer-links group relative w-fit py-1"href="/services/appdevelopment">App Development <div className="absolute left-0 bottom-0 h-1 w-0 bg-cyan-500 group-hover:w-full transition-all duration-500"></div> </Link>
+                                <Link className="footer-links group relative w-fit py-1"href="/services/bingads">Bing Ads<div className="absolute left-0 bottom-0 h-1 w-0 bg-cyan-500 group-hover:w-full transition-all duration-500"></div> </Link>
+                                <Link className="footer-links group relative w-fit py-1"href="/services/contentmarketing">Content Marketing<div className="absolute left-0 bottom-0 h-1 w-0 bg-cyan-500 group-hover:w-full transition-all duration-500"></div> </Link>
+                                <Link className="footer-links group relative w-fit py-1"href="/services/conversionrateoptimization">Conversion Rate Optimization<div className="absolute left-0 bottom-0 h-1 w-0 bg-cyan-500 group-hover:w-full transition-all duration-500"></div> </Link>
+                                <Link className="footer-links group relative w-fit py-1"href="/services/digitalmarketing">Digital Marketing<div className="absolute left-0 bottom-0 h-1 w-0 bg-cyan-500 group-hover:w-full transition-all duration-500"></div> </Link>
                             </ul>
                         </div>
                         <div >
                             {/* <h1 className="lg:text-xl text-cyan-400">Service</h1> */}
                             <ul className="flex flex-col">
-                                <Link className="footer-links group relative w-fit py-1"href="/services/emailmarketing">E-Mail Marketing<div className="absolute left-0 bottom-0 h-1 w-0 bg-white group-hover:w-full transition-all duration-500"></div> </Link>
-                                <Link className="footer-links group relative w-fit py-1"href="/services/facebookads">Facebook Ads<div className="absolute left-0 bottom-0 h-1 w-0 bg-white group-hover:w-full transition-all duration-500"></div> </Link>
-                                <Link className="footer-links group relative w-fit py-1"href="/services/googleads">Google Ads<div className="absolute left-0 bottom-0 h-1 w-0 bg-white group-hover:w-full transition-all duration-500"></div> </Link>
-                                <Link className="footer-links group relative w-fit py-1"href="/services/instagramads">Instagram Ads<div className="absolute left-0 bottom-0 h-1 w-0 bg-white group-hover:w-full transition-all duration-500"></div> </Link>
-                                <Link className="footer-links group relative w-fit py-1"href="/services/javascriptdevelopment">Javascript Development<div className="absolute left-0 bottom-0 h-1 w-0 bg-white group-hover:w-full transition-all duration-500"></div> </Link>
+                                <Link className="footer-links group relative w-fit py-1"href="/services/emailmarketing">E-Mail Marketing<div className="absolute left-0 bottom-0 h-1 w-0 bg-cyan-500 group-hover:w-full transition-all duration-500"></div> </Link>
+                                <Link className="footer-links group relative w-fit py-1"href="/services/facebookads">Facebook Ads<div className="absolute left-0 bottom-0 h-1 w-0 bg-cyan-500 group-hover:w-full transition-all duration-500"></div> </Link>
+                                <Link className="footer-links group relative w-fit py-1"href="/services/googleads">Google Ads<div className="absolute left-0 bottom-0 h-1 w-0 bg-cyan-500 group-hover:w-full transition-all duration-500"></div> </Link>
+                                <Link className="footer-links group relative w-fit py-1"href="/services/instagramads">Instagram Ads<div className="absolute left-0 bottom-0 h-1 w-0 bg-cyan-500 group-hover:w-full transition-all duration-500"></div> </Link>
+                                <Link className="footer-links group relative w-fit py-1"href="/services/javascriptdevelopment">Javascript Development<div className="absolute left-0 bottom-0 h-1 w-0 bg-cyan-500 group-hover:w-full transition-all duration-500"></div> </Link>
                             </ul>
                         </div>
                         <div >
                             {/* <h1 className="lg:text-xl text-cyan-400">Service</h1> */}
                             <ul className="flex flex-col">
-                                <Link className="footer-links group relative w-fit py-1"href="/services/landingpageoptimization">Landing Page Optimization<div className="absolute left-0 bottom-0 h-1 w-0 bg-white group-hover:w-full transition-all duration-500"></div> </Link>
-                                <Link className="footer-links group relative w-fit py-1"href="/services/linkedinmarketing">Linkedin Marketing<div className="absolute left-0 bottom-0 h-1 w-0 bg-white group-hover:w-full transition-all duration-500"></div> </Link>
-                                <Link className="footer-links group relative w-fit py-1"href="/services/marketingconsultation">Marketing Consultation<div className="absolute left-0 bottom-0 h-1 w-0 bg-white group-hover:w-full transition-all duration-500"></div> </Link>
-                                <Link className="footer-links group relative w-fit py-1"href="/services/seo">SEO<div className="absolute left-0 bottom-0 h-1 w-0 bg-white group-hover:w-full transition-all duration-500"></div> </Link>
-                                <Link className="footer-links group relative w-fit py-1"href="/services/socialmediamarketing">Social Media Marketing<div className="absolute left-0 bottom-0 h-1 w-0 bg-white group-hover:w-full transition-all duration-500"></div> </Link>
+                                <Link className="footer-links group relative w-fit py-1"href="/services/landingpageoptimization">Landing Page Optimization<div className="absolute left-0 bottom-0 h-1 w-0 bg-cyan-500 group-hover:w-full transition-all duration-500"></div> </Link>
+                                <Link className="footer-links group relative w-fit py-1"href="/services/linkedinmarketing">Linkedin Marketing<div className="absolute left-0 bottom-0 h-1 w-0 bg-cyan-500 group-hover:w-full transition-all duration-500"></div> </Link>
+                                <Link className="footer-links group relative w-fit py-1"href="/services/marketingconsultation">Marketing Consultation<div className="absolute left-0 bottom-0 h-1 w-0 bg-cyan-500 group-hover:w-full transition-all duration-500"></div> </Link>
+                                <Link className="footer-links group relative w-fit py-1"href="/services/seo">SEO<div className="absolute left-0 bottom-0 h-1 w-0 bg-cyan-500 group-hover:w-full transition-all duration-500"></div> </Link>
+                                <Link className="footer-links group relative w-fit py-1"href="/services/socialmediamarketing">Social Media Marketing<div className="absolute left-0 bottom-0 h-1 w-0 bg-cyan-500 group-hover:w-full transition-all duration-500"></div> </Link>
                             </ul>
                         </div>
                         <div >
                             {/* <h1 className="lg:text-xl text-cyan-400">Service</h1> */}
                             <ul className="flex flex-col">
-                                <Link className="footer-links group relative w-fit py-1"href="/services/webdesign">Web Design<div className="absolute left-0 bottom-0 h-1 w-0 bg-white group-hover:w-full transition-all duration-500"></div> </Link>
-                                <Link className="footer-links group relative w-fit py-1"href="/services/wordpressdevelopment">Wordpress Development<div className="absolute left-0 bottom-0 h-1 w-0 bg-white group-hover:w-full transition-all duration-500"></div> </Link>
-                                <Link className="footer-links group relative w-fit py-1"href="/services/youtubeads">Youtube Ads<div className="absolute left-0 bottom-0 h-1 w-0 bg-white group-hover:w-full transition-all duration-500"></div> </Link>
-                                {/* <Link className="footer-links group relative w-fit py-1"href="#">Content Marketing<div className="absolute left-0 bottom-0 h-1 w-0 bg-white group-hover:w-full transition-all duration-500"></div> </Link>
-                                <Link className="footer-links group relative w-fit py-1"href="#">App Marketing<div className="absolute left-0 bottom-0 h-1 w-0 bg-white group-hover:w-full transition-all duration-500"></div> </Link> */}
+                                <Link className="footer-links group relative w-fit py-1"href="/services/webdesign">Web Design<div className="absolute left-0 bottom-0 h-1 w-0 bg-cyan-500 group-hover:w-full transition-all duration-500"></div> </Link>
+                                <Link className="footer-links group relative w-fit py-1"href="/services/wordpressdevelopment">Wordpress Development<div className="absolute left-0 bottom-0 h-1 w-0 bg-cyan-500 group-hover:w-full transition-all duration-500"></div> </Link>
+                                <Link className="footer-links group relative w-fit py-1"href="/services/youtubeads">Youtube Ads<div className="absolute left-0 bottom-0 h-1 w-0 bg-cyan-500 group-hover:w-full transition-all duration-500"></div> </Link>
+                                {/* <Link className="footer-links group relative w-fit py-1"href="#">Content Marketing<div className="absolute left-0 bottom-0 h-1 w-0 bg-cyan-500 group-hover:w-full transition-all duration-500"></div> </Link>
+                                <Link className="footer-links group relative w-fit py-1"href="#">App Marketing<div className="absolute left-0 bottom-0 h-1 w-0 bg-cyan-500 group-hover:w-full transition-all duration-500"></div> </Link> */}
                             </ul>
                         </div>
 
